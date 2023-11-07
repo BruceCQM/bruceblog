4efe6e3a9a541681f08b58170a1add1918c2f5de
Kimmy <caiqimin@mucfc.com>
2023-11-07 14:40:34 +0800

    feat: push脚本修改

--- push.sh
+++ push.sh
@@ -2,5 +2,6 @@
 # 推送代码到github和Gitee
 # git push
 git push git@github.com:BruceCQM/bruceblog.git main:main
-git push git@gitee.com:brucecai55520/bruceblog.git main:bruceblog_vitepress
-# git push https://gitee.com/brucecai55520/bruceblog.git main:bruceblog_vitepress
+# git push git@gitee.com:brucecai55520/bruceblog.git main:bruceblog_vitepress
+# git push https://github.com/BruceCQM/bruceblog.git main:main
+git push https://gitee.com/brucecai55520/bruceblog.git main:bruceblog_vitepress
