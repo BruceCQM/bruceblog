fc2c0b145e39975ac5ee974514c5f462a71d507c
bruce <email>
2024-05-23 09:51:46 +0800

    revise push shell

--- push.sh
+++ push.sh
@@ -1,7 +1,10 @@
 # 执行该文件命令：sh ./push.sh
 # 推送代码到github和Gitee
 # git push
-git push git@github.com:BruceCQM/bruceblog.git main:main
 # git push git@gitee.com:brucecai55520/bruceblog.git main:bruceblog_vitepress
 # git push https://github.com/BruceCQM/bruceblog.git main:main
-git push https://gitee.com/brucecai55520/bruceblog.git main:master
+# git push git@github.com:BruceCQM/bruceblog.git main:main
+# git push https://gitee.com/brucecai55520/bruceblog.git main:master
+
+git push https://github.com/BruceCQM/bruceblog.git master:main
+git push https://gitee.com/brucecai55520/bruceblog.git master:master
