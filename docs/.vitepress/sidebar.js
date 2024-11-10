fc0f0355ddf17417de8729f8d8bb489f4cc832dc
Bruce <850611576@qq.com>
2024-11-10 23:37:07 +0800

    Fix error.

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -1,7 +1,7 @@
 import resources from '../resources/sidebar'
 import fe from '../fe/sidebar'
 import projects from '../projects/sidebar'
-import cs from '../cs/sidebar'
+// import cs from '../cs/sidebar'
 import lang from '../lang/sidebar'
 import magazine from '../magazine/sidebar'
 import work from '../work/sidebar'
@@ -11,7 +11,7 @@ export default {
   '/resources': resources,
   '/fe': fe,
   '/projects': projects,
-  '/cs': cs,
+  // '/cs': cs,
   '/lang': lang,
   '/magazine': magazine,
   '/work': work,
